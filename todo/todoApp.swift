eea1484a55b015782eb3975148ec1c882ea4e30e
MrFarhan <muhammadfarhan012345@gmail.com>
2024-08-20 22:10:30 +0500

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by mustafa on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
