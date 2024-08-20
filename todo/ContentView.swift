49bb291a7631bd7672e0f1a3be850590bba843bd
MrFarhan <muhammadfarhan012345@gmail.com>
2024-08-20 23:14:22 +0500

    Created basic button and circles with state and with animation props

--- todo/ContentView.swift
+++ todo/ContentView.swift
@@ -1,21 +1,43 @@
-//
-//  ContentView.swift
-//  todo
-//
-//  Created by mustafa on 20/08/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery","Baseball"]
+
+    @State var selected = "Baseball"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+ 
+        
+        VStack{
+            Text("My Swift App")
+                .font(.headline)
+                .foregroundColor(Color.green)
+                .multilineTextAlignment(.center)
+                .padding(0.0)
+            
+              
+            
+            Circle()
+                .fill(.blue)
+                .padding()
+                .overlay(
+                    Image(systemName:"figure.\(selected.lowercased())").font(
+                        .system(size:144)
+                    ).foregroundColor(.white)
+                )
+            Text("\(selected)123").font(.system(size:30))
+
         }
-        .padding()
+        
+
+        
+        Button("Next") {
+            
+            withAnimation(.easeInOut(duration: 1)) {
+                selected = activities.randomElement() ?? "Archery"
+            }
+            
+        }.buttonStyle(.borderedProminent)
     }
 }
 
